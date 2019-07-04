474ab0e91bf6198a647687fab9e1baa0964683aa
ATOM3S <jonykid-delaunay@hotmail.fr>
2019-07-04 19:07:42 +0200

    Ajout des contrôles à la souris

--- script.js
+++ script.js
@@ -39,6 +39,7 @@ var score = 0;
 // Évènements
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("mousemove", mouseMoveHandler, false);
 
 function keyDownHandler(e) {
     if(e.keyCode == 39) {
@@ -55,6 +56,13 @@ function keyUpHandler(e) {
     }
     else if(e.keyCode == 37) {
         leftPressed = false;
+    }
+}
+
+function mouseMoveHandler(e) {
+    var relativeX = e.clientX - canvas.offsetLeft;
+    if(relativeX > (0 + paddleWidth/2)  && relativeX < (canvas.width - paddleWidth/2)) {
+        paddleX = relativeX - paddleWidth/2;
     }
 }
 
